aac693cd84e0f73a59293a810b233f2d2d6e21c1
Bo Yu <w398700773@gmail.com>
2018-06-01 16:52:49 -0400

    Introduction Card

--- src/App.js
+++ src/App.js
@@ -9,9 +9,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My name is Bo</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div className="App-card">
+            <span className="name">Bo Yu</span>
+            <span className="slogen">//Research //Innovate //Create</span>
+          
+          <button className="getInTouch">GET IN TOUCH</button>
+          </div>
         </header>
+
         <p className="App-intro">
           
         </p>
