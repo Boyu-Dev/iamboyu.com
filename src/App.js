eaad700beff37b627bd47a384732035f1b659e1c
w398700773@gmail.com <w398700773@gmail.com>
2018-06-01 14:50:03 -0400

    changed font to quicksand

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"/>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">My name is Bo</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
