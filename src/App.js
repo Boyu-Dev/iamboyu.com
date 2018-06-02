7b40d93a3343e3621915659b5d0e63a1e81c8272
Bo Yu <w398700773@gmail.com>
2018-06-01 22:05:31 -0400

    sticky app-card

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
 class App extends Component {
+// Get the navbar
+
+  constructor() {
+    super();
+    this.state = {sticky :true};
+    this.isSticky = this.isSticky.bind(this);
+  }
+
+
+  
+  isSticky() {
+    this.setState({
+      blur : !this.state.blur
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
         <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"/>
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <div className="App-card">
-            <span className="name">Bo Yu</span>
-            <span className="slogen">//Research //Innovate //Create</span>
-          
-          <button className="getInTouch">GET IN TOUCH</button>
-          </div>
+        
         </header>
-
-        <p className="App-intro">
-          
+        <div className = {this.state.blur ? 'App-card':'sticky'} onScroll = {this.isSticky}>
+          <span className="name">Bo Yu</span>
+          <span className="slogen">//Research //Innovate //Create</span>
+          <button className="getInTouch">GET IN TOUCH</button>
+        </div>
+        <p className="App-intro">  
         </p>
       </div>
     );
