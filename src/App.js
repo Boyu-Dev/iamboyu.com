99b7e0428b18ce9de49ed092241a2345a26befea
Bo Yu <yuboize@gmail.com>
2018-06-05 21:02:17 -0400

    fixed and actually get what i am doing with page offset....

--- src/App.js
+++ src/App.js
@@ -4,19 +4,43 @@ import './App.css';
 
 class App extends Component {
 // Get the navbar
-
   constructor() {
     super();
-    this.state = {sticky :true};
-    this.isSticky = this.isSticky.bind(this);
+    this.state = {
+      stickNav: false,
+      threshhold: Number.POSITIVE_INFINITY
+    };
   }
 
+  componentDidMount(){
+    window.addEventListener("scroll", this.handleWindowScroll);
+  }
+  componentWillUnmount(){
+    window.removeEventListener("scroll", this.handleWindowScroll);
+  }
 
-  
-  isSticky() {
-    this.setState({
-      blur : !this.state.blur
-    })
+  handleWindowScroll = (e) => {
+    if(!this.navBar){
+      return;
+    }
+
+    const pageYOffset = window.pageYOffset;
+
+    if (pageYOffset > 0 ){
+      this.setState({stickNav: true})
+      console.log(pageYOffset);
+    }
+    else{
+      this.setState({stickNav: false})
+    }
+  }
+
+  getNavBarStyle(){
+    return this.state.stickNav ? 'App-cardSticky' : 'App-card';
+
+  }
+  setNameStyle(){
+    return this.state.stickNav ? { size: '20pt'} : {size: '70pt'};
   }
 
   render() {
@@ -27,11 +51,13 @@ class App extends Component {
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
         
         </header>
-        <div className = {this.state.blur ? 'App-card':'sticky'} onScroll = {this.isSticky}>
+
+        <div className={this.getNavBarStyle()} ref={ref=>this.navBar=ref}>
           <span className="name">Bo Yu</span>
           <span className="slogen">//Research //Innovate //Create</span>
           <button className="getInTouch">GET IN TOUCH</button>
         </div>
+
         <p className="App-intro">  
         </p>
       </div>
