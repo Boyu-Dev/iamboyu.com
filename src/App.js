5ac5aaa3e3184fb392bd81725ec686795bd93d73
Bo Yu <yuboize@gmail.com>
2018-06-13 19:59:34 -0400

    new page layout

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ class App extends Component {
     super();
     this.state = {
       stickNav: false,
+      isToggleOn: false,
+      atHome: false,
+      atAbout: false,
+      atWork: false,
+      atExp: false,
     };
+
+    this.tabOnClick = this.tabOnClick.bind(this);
+    this.getContact = this.getContact.bind(this);
   }
 
   componentDidMount(){
@@ -23,33 +31,58 @@ class App extends Component {
     if(!this.navBar){
       return;
     }
-
     const pageYOffset = window.pageYOffset;
+   
+    if(pageYOffset < 1){
+      this.setState({
+        atHome: true,
+        atAbout: false,
+        atWork: false,
+        atExp: false,
+        stickNav: false
+      });
+    }
+    else if (pageYOffset > 0){
+      this.setState({stickNav: true});
+    }
+  }
 
-    if (pageYOffset > 0 ){
-      this.setState({stickNav: true})
-      //console.log(pageYOffset);
-    }
-    else{
-      this.setState({stickNav: false})
-    }
+  tabOnClick(){
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn,
+      stickNav: !prevState.stickNav,
+    }));
   }
 
   getNavBarStyle(){
-    return this.state.stickNav ? 'App-cardSticky' : 'App-card';
+    return !this.state.stickNav ?  'App-card' : 'App-cardSticky';
 
   }
   setNameStyle(){
-    return this.state.stickNav ?  'nameSticky' : 'name' ;
+    return !this.state.stickNav ?  'name' : 'nameSticky' ;
   }
 
-  setSlogenStyle(){
-    return this.state.stickNav ? 'slogenSticky' : 'slogenSticky alined'; 
+  setTabStyle(){
+    return !this.state.stickNav ? 'tabSticky tabTaggleOff' : 'tabSticky tabTaggleOn' ; 
   }
   setContactButtonStyle(){
-    return this.state.stickNav ? 'getInTouchSticky' : 'getInTouch';
+    return !this.state.stickNav ? 'getInTouch' : 'getInTouchSticky';
+  }
+  setTagline(){
+    return !this.state.stickNav ? 'tagline' : 'taglineSticky' ;
   }
 
+
+  getContact(){
+    console.log("contact!");
+    this.render(
+      <div className="contactPage">
+      hi
+      </div>
+
+
+    );
+  }
 
 
   render() {
@@ -58,41 +91,42 @@ class App extends Component {
         <header className="App-header">
         <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"/>
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        
         </header>
        
 
         <div className={this.getNavBarStyle()} ref={ref=>this.navBar=ref}>
-          <span className={this.setNameStyle()}>HELLO</span>
-          <span className={this.setSlogenStyle()}>
+          <span className={this.setNameStyle()}>Bo Yu</span>
+          <span className={this.setTagline()}>Interactive Software Developer</span>
+          <span className={this.setTabStyle()}>
            <ul>
-            <li className="tab">
+            <li className="tab" onClick={this.tabOnClick}>
               <div className="shiftLeft">
-                <div class="initText">//About</div>
+                <div className="initText">//About</div>
               </div>
               <div className="shiftRight">
-                <div class="addtext">Me</div>
+                <div className="addtext">Me</div>
               </div>
             </li>
             <li className="tab">
             <div className="shiftLeft">
-                <div class="initText">//Skilled</div>
+                <div className="initText">//Work</div>
               </div>
               <div className="shiftRight">
-                <div class="addtext">In</div>
+                <div className="addtext">. I Did</div>
               </div>
             </li>
             <li className="tab">
-            <div className="shiftLeft">
-                <div class="initText">//Exper</div>
+            <div className="shiftLeft exp">
+                <div className="initText">//Exp</div>
               </div>
-              <div className="shiftRight">
-                <div class="addtext">-ience</div>
+              <div className="shiftRight erience">
+                <div className="addtext">--.erience</div>
               </div>
             </li>
            </ul>
+           <div></div>
           </span>
-          <button className={this.setContactButtonStyle()}>GET IN TOUCH</button>
+          <button className={this.setContactButtonStyle()} onClick={this.getContact}>GET IN TOUCH</button>
         </div>
 
         <p className="App-intro">  
