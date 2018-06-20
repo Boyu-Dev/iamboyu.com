fb84b31f5c0d7d662151183a06d98699234b3671
Bo Yu <yuboize@gmail.com>
2018-06-19 20:29:09 -0400

    about me page setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+
 
 
 class App extends Component {
@@ -93,7 +93,7 @@ class App extends Component {
 
         <div className={this.getNavBarStyle()} ref={ref=>this.navBar=ref}>
           <span className={this.setNameStyle()}>Bo Yu</span>
-          <span className={this.setTagline()}>Interactive Software Developer & UX Engineer</span>
+          <span className={this.setTagline()}>UX Engineer && Software Developer </span>
           <span className={this.setTabStyle()}>
            <ul>
             <li className="tab" onClick={this.tabOnClick}>
@@ -104,14 +104,7 @@ class App extends Component {
                 <div className="addtext">Me</div>
               </div>
             </li>
-            <li className="tab">
-            <div className="shiftLeft">
-                <div className="initText">//Work</div>
-              </div>
-              <div className="shiftRight">
-                <div className="addtext">. I Did</div>
-              </div>
-            </li>
+        
             <li className="tab">
             <div className="shiftLeft exp">
                 <div className="initText">//Exp</div>
@@ -120,16 +113,84 @@ class App extends Component {
                 <div className="addtext">--.erience</div>
               </div>
             </li>
+
+            <li className="tab">
+              <div className="shiftLeft">
+                <div className="initText">//Work</div>
+              </div>
+              <div className="shiftRight">
+                <div className="addtext">. I Did</div>
+              </div>
+            </li>
+
            </ul>
-           <div></div>
+           <div className="boInfo">
+            {this.state.stickNav ? 
+              <div className="aboutPage">
+                <div className="aboutBackdrop">
+                  About
+                </div>
+                <div className="AboutIntro-text">
+                  <h>Me</h>
+                  I am Bo Yu, a 24-year-old Canadian 
+                  <strong> UX Engineer and Software Developer. </strong>
+                    I love making things that people interact with and 
+                  I am always learning something new 
+                  <strong> (I am currently learning about UX in games). </strong>
+                  I like to <strong> overcome </strong> design challenages,
+                  <strong> create </strong> intelligent user experiences and software
+                  with strong integrity. 
+                  <br/>
+                  Now days when I have free time, I am like to make silly games. And 
+                  Once a year I travel the world to <strong> experience and learn </strong> 
+                  about new cultures.
+                </div>
+                  <div className="About-skills">
+                    <h>My Skills</h>
+                    <div className="skill-App a1">
+                    hi
+                    </div>
+                    <div className="skill-App a2">
+                    hi
+                    </div>
+                    <div className="skill-App a3">
+                    hi
+                    </div>
+                    <div className="skill-App a4">
+                    hi
+                    </div>
+                    <div className="skill-App a5">
+                    hi
+                    </div>
+                    <div className="skill-App a6">
+                    hi
+                    </div>
+                    <div className="skill-App a7">
+                    hi
+                    </div>
+                    <div className="skill-App a8">
+                    hi
+                    </div>
+                    <div className="skill-App a9">
+                    hi
+                    </div>
+                    <div className="skill-App a10">
+                    hi
+                    </div>
+                    <div className="skill-App a11">
+                    hi
+                    </div>
+                    <div className="skill-App a12">
+                    hi
+                    </div>
+                  </div>
+              </div>
+            : null}
+             
+           </div>
           </span>
           <button className={this.setContactButtonStyle()} onClick={this.getContact}>GET IN TOUCH</button>
             {this.state.getContact ? 
-              <ReactCSSTransitionGroup
-                transitionName="contactPageAnimation"
-                transitionEnterTimeout={500}
-                transitionLeaveTimeout={300}
-              >
               <div className="contactPage">
                 <div className="closeContactPage" onClick={this.getContact}>X</div>
                 <p className="contactTitle">Let's Talk</p>
@@ -149,11 +210,9 @@ class App extends Component {
                   </div>
                 </form>
               </div>
-              </ReactCSSTransitionGroup> : null}
+              : null}
         </div>
 
-        <p className="App-intro">  
-        </p>
       </div>
     );
   }
